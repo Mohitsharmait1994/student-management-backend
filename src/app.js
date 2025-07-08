ab756f7192d6d07344b8b81995925b1832340156
Mohitsharmait1994 <mohitsharmait1994@gmail.com>
2025-07-08 17:47:42 +0530

    changes in app.js

--- src/app.js
+++ src/app.js
@@ -21,6 +21,6 @@ sequelize.authenticate()
   })
   .then(() => {
     console.log('✅ Models synchronized.');
-    app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
+    app.listen(PORT, '0.0.0.0', () => console.log(`🚀 Server running on port ${PORT}`));
   })
   .catch(err => console.error('❌ Startup error:', err));
